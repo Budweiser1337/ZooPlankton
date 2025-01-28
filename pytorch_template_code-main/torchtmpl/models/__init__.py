b7acf91b128dd2139ea358550475ca9b746bb3f4
Yvan <yvan.hu@student-cs.fr>
2025-01-28 19:54:20 +0100

    simple U-Net

--- pytorch_template_code-main/torchtmpl/models/__init__.py
+++ pytorch_template_code-main/torchtmpl/models/__init__.py
@@ -6,8 +6,8 @@ import torch
 # Local imports
 from .base_models import *
 from .cnn_models import *
-# from .UNet import UNet
-from .UNet_improved import UNet
+from .UNet import UNet
+# from .UNet_improved import UNet
 
 def build_model(cfg, input_size, num_classes):
     return eval(f"{cfg['class']}(cfg, input_size, num_classes)")
