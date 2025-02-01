54422287157b30f54adcb32548dd4b4c4836c326
Yvan <yvan.hu@student-cs.fr>
2025-02-01 23:54:59 +0100

    unet

--- pytorch_template_code-main/torchtmpl/main.py
+++ pytorch_template_code-main/torchtmpl/main.py
@@ -52,23 +52,23 @@ def train(config):
     # Build the model
     logging.info("= Model")
     model_config = config["model"]
-    # model = models.build_model(model_config, input_size[0], 1)
+    model = models.build_model(model_config, input_size[0], 1)
     # model.load_state_dict(torch.load("/usr/users/sdim/sdim_22/team-6-kaggle-challenge-deep-learning/pytorch_template_code-main/model_logs/UNet_3/best_model.pt")) 
     # Load DeepLabV3+ with ResNet backbone
-    model = torchmodels.deeplabv3_resnet50(pretrained=False)
-    num_classes = 1
-    model.classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1))
-    resnet_backbone = model.backbone
+    # model = torchmodels.deeplabv3_resnet50(pretrained=False)
+    # num_classes = 1
+    # model.classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1))
+    # resnet_backbone = model.backbone
 
-    # Modify the first convolutional layer to accept 1 channel
-    resnet_backbone.conv1 = torch.nn.Conv2d(
-        in_channels=1,
-        out_channels=resnet_backbone.conv1.out_channels,
-        kernel_size=resnet_backbone.conv1.kernel_size,
-        stride=resnet_backbone.conv1.stride,
-        padding=resnet_backbone.conv1.padding,
-        bias=resnet_backbone.conv1.bias is not None
-    )
+    # # Modify the first convolutional layer to accept 1 channel
+    # resnet_backbone.conv1 = torch.nn.Conv2d(
+    #     in_channels=1,
+    #     out_channels=resnet_backbone.conv1.out_channels,
+    #     kernel_size=resnet_backbone.conv1.kernel_size,
+    #     stride=resnet_backbone.conv1.stride,
+    #     padding=resnet_backbone.conv1.padding,
+    #     bias=resnet_backbone.conv1.bias is not None
+    # )
     model.to(device)
 
     # Build the loss
