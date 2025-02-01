dd7c8941b40af9c7ea24a7dbfce83b58ec5d4d28
Yvan <yvan.hu@student-cs.fr>
2025-02-01 23:57:08 +0100

    unet w/ dropout

--- pytorch_template_code-main/torchtmpl/models/UNet.py
+++ pytorch_template_code-main/torchtmpl/models/UNet.py
@@ -56,9 +56,9 @@ class UNet(nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            # nn.Dropout2d(0.5),
+            nn.Dropout2d(0.3),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            # nn.Dropout2d(0.5)
+            nn.Dropout2d(0.3)
         )
