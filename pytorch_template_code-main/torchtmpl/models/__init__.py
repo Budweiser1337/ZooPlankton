1090122ecc40af014291006c25268042a62dbe51
Yvan <yvan.hu@student-cs.fr>
2025-02-06 00:11:19 +0100

    deeplabv3

--- pytorch_template_code-main/torchtmpl/models/__init__.py
+++ pytorch_template_code-main/torchtmpl/models/__init__.py
@@ -2,6 +2,7 @@
 
 # External imports
 import torch
+import torchvision.models.segmentation as torchmodels
 
 # Local imports
 from .base_models import *
@@ -10,4 +11,20 @@ from .UNet import UNet
 # from .UNet_improved import UNet
 
 def build_model(cfg, input_size, num_classes):
+    # Load DeepLabV3+ with ResNet backbone
+    if cfg['class'] == "DeepLabV3":
+        model = torchmodels.deeplabv3_resnet50(pretrained=False)
+        model.classifier[4] = torch.nn.Conv2d(256, num_classes, kernel_size=(1, 1))
+        resnet_backbone = model.backbone
+
+        # Modify the first convolutional layer to accept 1 channel
+        resnet_backbone.conv1 = torch.nn.Conv2d(
+            in_channels=1,
+            out_channels=resnet_backbone.conv1.out_channels,
+            kernel_size=resnet_backbone.conv1.kernel_size,
+            stride=resnet_backbone.conv1.stride,
+            padding=resnet_backbone.conv1.padding,
+            bias=resnet_backbone.conv1.bias is not None
+        )
+        return model
     return eval(f"{cfg['class']}(cfg, input_size, num_classes)")
