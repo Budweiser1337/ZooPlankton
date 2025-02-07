edeafaebcaf0bb8e5d70084a88b61c02cf8667e0
Yvan <yvan.hu@student-cs.fr>
2025-02-07 23:44:34 +0100

    fix scaler

--- pytorch_template_code-main/torchtmpl/utils.py
+++ pytorch_template_code-main/torchtmpl/utils.py
@@ -101,7 +101,7 @@ def train(model, loader, f_loss, optimizer, device, config, dynamic_display=True
         # Backward and optimize
         optimizer.zero_grad(set_to_none=True)
         scaler.scale(loss).backward()
-        optimizer.step()
+        scaler.step(optimizer)
         scaler.update()
                 
         # Update the metrics
